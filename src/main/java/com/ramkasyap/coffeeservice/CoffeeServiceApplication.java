ade39443fd454d63fa125e2a37ecc512a81cb3c1
ramkasyap <ram.k@turtlemint.com>
2018-10-27 16:03:16 +0530

    first commit

---
+++ src/main/java/com/ramkasyap/coffeeservice/CoffeeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ramkasyap.coffeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeServiceApplication.class, args);
+	}
+}
